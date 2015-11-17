dcd7133e6f3420e3a40a8b0ad9f5915b646d72a4
Pablo Prieto <pprieto01@citymail.cuny.edu>
2015-11-17 18:45:56 -0500

    removed esport_tutor folder

--- esport_tutor/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Derrick/Documents/CSC473/webdesignproject/esport_tutor/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
