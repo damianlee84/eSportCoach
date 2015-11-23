7c9a1093979e4db6afb1086b1b687fb339f0b23e
fchen00 <fchen00@citymail.cuny.edu>
2015-11-23 16:18:20 -0500

    Inheritance where partent User with children Coach Tutee

--- esport_coach/esport_coach/settings.py
+++ esport_coach/esport_coach/settings.py
@@ -95,7 +95,22 @@ EMAIL_USE_TLS = True
 #         }
 #     }
 
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'testin',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'feiyu',
+            'PASSWORD': 'abc1234',
+            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '5432',                      # Set to empty string for default.
+        }
+    }
 
+
+
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -106,6 +121,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
 
 
--- esport_coach/sideapp/models.py
+++ esport_coach/sideapp/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 class Signup(models.Model):
     email = models.EmailField()
-    full_name = models.CharField(max_length=50, blank = False, null=False)
-    timestamp = models.DateTimeField(auto_now_add = True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add = False, auto_now="True")
+    full_name = models.CharField(max_length=50, blank=False, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now="True")
 
-    mmr = models.CharField(max_length=50, blank = False, null=False)
-    server = models.CharField(max_length=50, blank = False, null=False)
-    hero = models.CharField(max_length=50, blank = False, null=False)
-    rating = models.PositiveIntegerField(blank = False, null=False)
-    reputation = models.PositiveIntegerField(blank = False, null=False)
-    students = models.CharField(max_length=50, blank = False, null=False)
+    mmr = models.CharField(max_length=50, blank=False, null=False)
+    server = models.CharField(max_length=50, blank=False, null=False)
+    hero = models.CharField(max_length=50, blank=False, null=False)
+    rating = models.PositiveIntegerField(blank=False, null=False)
+    reputation = models.PositiveIntegerField(blank=False, null=False)
+    students = models.CharField(max_length=50, blank=False, null=False)
     pricerate = models.PositiveIntegerField(default=0)
 
     def __unicode__(self):
@@ -22,28 +22,29 @@ class Signup(models.Model):
 class User(models.Model):
     username = models.CharField(primary_key=True, max_length = 100, blank = False, null = False)
     password = models.CharField(max_length = 100, blank = False, null = False)
-    email = models.CharField(max_length = 100, blank = False, null = False)
+    email = models.EmailField(blank = False, null = False)
     name = models.CharField(max_length = 100, blank = False, null = False)
     def __str__(self):
         return self.username + " " + self.email + " " + self.name
 
-class Coach(models.Model):
-   username = models.ForeignKey(User)
+class Coach(User):
    MMR = models.IntegerField(default=0,  blank = False, null = False)
    hero = models.CharField(max_length = 500, blank = False, null = False)
-   rank = models.IntegerField(default = 0)
    server = models.CharField(max_length = 50, blank = False, null = False)
-   rating = models.IntegerField(default = 5,  blank = False, null = False)
+   rating = models.IntegerField(default = 0,  blank = False, null = False)
    hour_rate = models.FloatField(default = 0.00)
    def __str__(self):
-        return self.username + " " + self.hero + " " + self.rank + " " + self.server + " " + self.server + " " + self.hour_rate
+        return self.username + " " + self.hero + " " + str(self.rating) + " " + self.server + " " + str(self.hour_rate)
 
-class Tutee(models.Model):
-    username = models.ForeignKey(User)
+class Tutee(User):
+   def __str__(self):
+        return self.username + " " + self.email + " " + self.name
 
-class Admin(models.Model):
-    username = models.ForeignKey(User)
 
-class register(models.Model):
+
+
+class Register(models.Model):
     username = models.ForeignKey(User)
     rdate = models.DateTimeField(auto_now_add = False, auto_now = "True")
+    def __str__(self):
+      return self.username + " " + rdate
