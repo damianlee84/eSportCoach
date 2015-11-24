ad2f783b18edc2d2779827f903bb3b0792097388
WaiLoon_Chong <devnull@localhost>
2015-11-23 22:42:53 -0500

    Stripe API implementation #3

--- esport_coach/sideapp/forms.py
+++ esport_coach/sideapp/forms.py
@@ -34,11 +34,58 @@ class CreditCardField(forms.IntegerField):
         return super(CreditCardField, self).clean(value)
     
 class expDate(forms.MultiWidget):
-    def inExp_date(self, value):
+    def decompress(self, value):
         return [value.month, value.year] if value else [None, None]
  
     def format_output(self, rendered_widgets):
         html = u' / '.join(rendered_widgets)
         return u'<span style="white-space: nowrap;">%s</span>' % html 
 
-
+    
+"""code snippet from STRIPE documentation """
+"""user can select the months and years from the given selection in the selection boxes"""
+class expDateInput(forms.MultiValueField):
+    EXP_MONTH = [(x, x) for x in xrange(1, 13)]
+    EXP_YEAR = [(x, x) for x in xrange(date.today().year,
+                                       date.today().year + 15)]
+    default_error_messages = {
+        'invalid_month': u'Enter a valid month.',
+        'invalid_year': u'Enter a valid year.',
+    }
+ 
+    def __init__(self, *args, **kwargs):
+        errors = self.default_error_messages.copy()
+        if 'error_messages' in kwargs:
+            errors.update(kwargs['error_messages'])
+        fields = (
+            forms.ChoiceField(choices=self.EXP_MONTH,
+                error_messages={'invalid': errors['invalid_month']}),
+            forms.ChoiceField(choices=self.EXP_YEAR,
+                error_messages={'invalid': errors['invalid_year']}),
+        )
+        super(CCExpField, self).__init__(fields, *args, **kwargs)
+        self.widget = CCExpWidget(widgets =
+            [fields[0].widget, fields[1].widget])
+ 
+    def clean(self, value):
+        exp = super(CCExpField, self).clean(value)
+        if date.today() > exp:
+            raise forms.ValidationError(
+            "The expiration date you entered is in the past.")
+        return exp
+ 
+    def compress(self, data_list):
+        if data_list:
+            if data_list[1] in forms.fields.EMPTY_VALUES:
+                error = self.error_messages['invalid_year']
+                raise forms.ValidationError(error)
+            if data_list[0] in forms.fields.EMPTY_VALUES:
+                error = self.error_messages['invalid_month']
+                raise forms.ValidationError(error)
+            year = int(data_list[1])
+            month = int(data_list[0])
+            # find last day of the month
+            day = monthrange(year, month)[1]
+            return date(year, month, day)
+        return None
+    
