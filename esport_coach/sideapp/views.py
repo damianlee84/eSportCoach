5cad83a5bc5507a7e633e1cf58d608ab5011bf5b
wantong <devnull@localhost>
2015-11-27 12:37:41 -0500

    fixed errors, api works but still need more work

--- esport_coach/sideapp/views.py
+++ esport_coach/sideapp/views.py
@@ -30,13 +30,7 @@ def login(request):
     # context = RequestContext(request, {
     #     'request': request, 'user': request.user})
     # return render_to_response('login.html', context_instance=context)
-    return render(request, 'login.html')
-
-
-@login_required(login_url='/')
-def home(request):
-    return render_to_response('home.html')
-
+    return render(request, 'signup.html')
 
 def logout(request):
     auth_logout(request)
